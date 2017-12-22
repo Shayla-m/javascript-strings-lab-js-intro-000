fee40f133c2c708a5b2ca0758557ff328f1d980f
shayla mujic <shaylamujic@yahoo.com>
2017-12-22 18:05:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var greeting = "Hello, everybody!";
 var greeting - "Hello, everybody!" ;
 
 var specialGuest = "Neil deGrasse Tyson"
+var specialGuest - "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "neil degrasse tyson" + specialGuest + "!";
+var greetSpecialGuest - "neil degrasse tyson" + specialGuest + "!" ;
 
 var topic = "space";
+var topic - "space" ;
 
 var conversation = `${topic}`;
+var conversation = '${topic}'; 
